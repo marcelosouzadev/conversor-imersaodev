6896826b5bba5603f4507db0bc10d22935c7d26e
Marcelo Souza <81497485+marcelosouzadev@users.noreply.github.com>
2021-03-27 23:34:26 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,13 @@
+const conversor = document.querySelector('button')
+
+conversor.onclick = () => 
+{
+  var ValorMilhas = Number(document.querySelector('input').value.replace('.',','))
+  if (ValorMilhas < 1) 
+  {
+document.querySelector('span').innerHTML = "Valor inválido!"
+  } 
+  else {
+document.querySelector('span').innerHTML = 'Resultado: ' + (ValorMilhas * 0.62) + ' milhas. ' + ' Deixe seu like se gostou. Obrigado, Marcelo!'
+  }  
+}
