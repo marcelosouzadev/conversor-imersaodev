39ec71df092a2b73377a614db0748557824a6dfc
Marcelo Souza <81497485+marcelosouzadev@users.noreply.github.com>
2021-03-28 21:47:53 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,13 +1,12 @@
-const conversor = document.getElementById('[button]')
+document.getElementById("botao").onclick = function() {
 
-conversor.onclick = ('[button]') 
-{
-  var ValorKm = Number(document.querySelector('[input]').value.replace('.',','))
+  var ValorKm = Number(document.querySelector('input').value.replace('.',','))
+  
   if (ValorKm < 1) {
-document.querySelector('[span]').innerHTML = "Valor inválido!"
+document.querySelector('span').innerHTML = "Valor inválido!"
   } 
   else {
-document.querySelector('[span]').innerHTML = 'Resultado: ' + (ValorKm * 0.62) + ' milhas' 
+document.querySelector('span').innerHTML = 'Resultado: ' + (ValorKm * 0.62) + ' milhas' 
   }  
 }
 
