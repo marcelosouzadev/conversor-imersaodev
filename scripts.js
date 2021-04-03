679282c22d01dab90d2f17e57933b110a2b23778
Marcelo Souza <81497485+marcelosouzadev@users.noreply.github.com>
2021-04-03 18:32:43 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@ const valorFixoKm = 0.62
 var kmParaMilhas = (valorEmKm * valorFixoKm)
 
 if (kmParaMilhas) {
-document.getElementById('resultadoMilhas').innerHTML = "O resultado é: " + kmParaMilhas + " milhas"
+document.getElementById('resultadoMilhas').innerHTML = "O resultado é: " + kmParaMilhas.toLocaleString('pt-BR') + " milhas"
 }                                             
  else {
     document.getElementById('resultadoMilhas').innerHTML = "Valor inválido!"
